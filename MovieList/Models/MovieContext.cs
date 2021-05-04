9a086dfe495996c624d37ad2bef59827bbfb3dd9
keira <keirakaitlynn@gmail.com>
2021-05-04 10:08:53 -0700

    Step 7-10: Code the classes for the model and the DB context (Part 1)

---
+++ MovieList/Models/MovieContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace MovieList.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        { }
+        public DbSet<Movie> Movies { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(
+                new Movie
+                {
+                    MovieId = 4,
+                    Name = "Casablanca",
+                    Year = 1943,
+                    Rating = 5,
+                },
+                new Movie
+                {
+                    MovieId = 2,
+                    Name = "Wonder Woman",
+                    Year = 2017,
+                    Rating = 3,
+                },
+                new Movie
+                {
+                    MovieId = 3,
+                    Name = "Moonstruck",
+                    Year = 1988,
+                    Rating = 4,
+                }
+            );
+        }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
